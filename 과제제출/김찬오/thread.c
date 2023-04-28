1e87dd83f6b09db58b661b9313d68829f5cb9353
caesar9132 <129859262+caesar9132@users.noreply.github.com>
2023-04-28 18:51:59 +0900

    Update thread.c
    뮤텍스를 사용하여 배타제어를 실행했습니다

--- 과제제출/김찬오/thread.c
+++ 과제제출/김찬오/thread.c
@@ -1,46 +1,32 @@
-		#include <stdio.h>
-		#include <pthread.h>
+			#include <pthread.h>
 	
-		void *print_t1(void *arg) {
-			
-	    printf("t1\n");
-	    pthread_exit(NULL);
-	 	pthread_exit(NULL);
-		    
+		pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	
+		void* print_t1(void* arg) {
+	    	pthread_mutex_lock(&mutex);
+	    	printf("t1\n");
+	    	pthread_mutex_unlock(&mutex);
+	    	return NULL;
+		}
+		
+		void* print_t2(void* arg) {
+	    	pthread_mutex_lock(&mutex);
+	    	printf("t2\n");
+	    	pthread_mutex_unlock(&mutex);
+	    	return NULL;
 		}
 	
-		void *print_t2(void *arg) {
-	    printf("t2\n");
-	    pthread_exit(NULL);
-			}
-		
 		int main() {
-	    pthread_t thread1, thread2;
-	    int rc;
-	    // t1 스레드만둘기 
-	    rc = pthread_create(&thread1, NULL, print_t1, NULL);
-	    if (rc) {
-	        printf("Error: Return code from pthread_create() is %d\n", rc);
-	        return 1;
-	    }
+	    	pthread_t thread1, thread2;
 	
-	    // t2 스레드  만둘기 
-	    rc = pthread_create(&thread2, NULL, print_t2, NULL);
-	    if (rc) {
-	    	
-	        printf("Error: Return code from pthread_create() is %d\n", rc);
-	        return 1;
-	        	
-	    	
-	    }
-	    pthread_join(thread1, NULL);
-	    pthread_join(thread2, NULL);
+	    	pthread_create(&thread1, NULL, &print_t1, NULL);
+	    	pthread_create(&thread2, NULL, &print_t2, NULL);
+	
+	    	pthread_join(thread1, NULL);
+	    	pthread_join(thread2, NULL);
 		
-	    return 0;
-	    
+	   	 return 0;
 		}
-	
-	
 	
 	
 	
