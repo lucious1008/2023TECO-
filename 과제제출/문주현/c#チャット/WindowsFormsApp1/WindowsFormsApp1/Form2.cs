644b27f4eaa67c930fac76bb92f5cf3a4ee13bc1
moonjuhyun <answngusdld@gmail.com>
2023-05-19 18:58:13 +0900

    "20230519"チャットc#の資料ムン・ジュヒョン

---
+++ 과제제출/문주현/c#チャット/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        string strConn = "Server=localhost;Port=3306;Database=mydb;Uid=root;Pwd=Root";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(strConn);
+            string insertQuery = "insert into user(id,pw)values('asdfa',1234)";
+            try
+            {
+                conn.Open();
+                MySqlCommand command = new MySqlCommand(insertQuery, conn);
+
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    Console.WriteLine("成功");
+                }
+                else
+                {
+                    Console.WriteLine("失敗");
+                }
+                conn.Close();
+                }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //id確認
+        }
+    }
+}
---
+++ 과제제출/문주현/c#チャット/WindowsFormsApp1/WindowsFormsApp1/Form3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            //pannel
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            //scroll < >
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            //scroll ^ 
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //chatlist
+        }
+    }
+}
